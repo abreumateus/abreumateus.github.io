581b4bbc3119c21ae11dd385bca778a67179d51e
Mateus Abreu de Andrade <mateusabreu@outlook.com>
2020-04-12 23:10:59 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,105 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  // list of colours for link hover states
-  var colors = ['#ff46a2', '#ff9630', '#32d476', '#ec2945', '#307fff'];
-  var last_color;
-  var prev_bg;
-  var random;
+    // link hover state
+    $('a:not(.lightbox)').mouseenter(function() {
+        $(this).css('color', 'white');
+        $(this).css('background-color', 'purple');
+        $(this).css('border-bottom-color', 'purple');
+        $(this).css('text-decoration', 'none');
+    });
 
-  // link hover state
-  $('a:not(.lightbox)').mouseenter(function () {
-    prev_bg = this.style.color;
-    do {
-      random = colors[Math.floor(Math.random() * colors.length)];
-    } while (random == last_color);
-    $(this).css('color', 'white');
-    $(this).css('background-color', random);
-    $(this).css('border-bottom-color', random);
-    $(this).css('text-decoration', 'none');
-    last_color = random;
-  });
+    // undo link hover state
+    $('a:not(.lightbox)').mouseleave(function() {
+        $(this).css('color', 'white');
+        $(this).css('background-color', 'black');
+        $(this).css('border-bottom-color', 'white');
+        $(this).css('text-decoration', 'none');
+    });
 
-  // undo link hover state
-  $('a:not(.lightbox)').mouseleave(function () {
-    $(this).css('color', 'white');
-    $(this).css('background-color', 'black');
-    $(this).css('border-bottom-color', 'white');
-    $(this).css('text-decoration', 'none');
-  });
+    // resets hover states on browser back button press
+    $(window).bind("pageshow", function(event) {
+        if (event.originalEvent.persisted) {
+            window.location.reload();
+        }
+    });
 
-  // resets hover states on browser back button press
-  $(window).bind("pageshow", function (event) {
-    if (event.originalEvent.persisted) {
-      window.location.reload();
-    }
-  });
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 100,
+                "density": {
+                    "enable": true,
+                    "value_area": 1000
+                }
+            },
+            "color": {
+                "value": "#9400D3"
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
 
 });
